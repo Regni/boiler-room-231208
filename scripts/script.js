e1cae8eb968a9af207e8bad0c13adf4914259854
Alexander Kuiper <kuiper9000@gmail.com>
2023-12-08 11:44:39 +0100

    filter buttons working

--- scripts/script.js
+++ scripts/script.js
@@ -34,16 +34,25 @@ function renderAll() {
 
   card.innerHTML = allItems.join("");
 }
-const categoryBTN = document.querySelector(".categoryButton");
+const categoryBTN = document.querySelector(".categories");
 categoryBTN.addEventListener("click", (e) => {
-  console.log(e);
-  filter(e.target.textContent.toLocaleLowerCase());
+  console.log(e.target.className);
+  if (e.target.className != "categories") {
+    filter(e.target.textContent.toLocaleLowerCase());
+  }
 });
 
 function filter(category) {
-  const filteredItems = products.filter((e) => e.category == category);
-  card.innerHTML = filteredItems.map(
-    (item) =>
-      `<div><h3>${item.name}</h3> <p>${item.price}</p> <button id = btn${item.id}> Köp! </button></div> `
-  );
+  console.log(category);
+  if (category != "ta bort filter") {
+    const filteredItems = products.filter((e) => e.category == category);
+    card.innerHTML = filteredItems
+      .map(
+        (item) =>
+          `<div class = "gridItemContainer"><h3>${item.name}</h3> <div class = "itemPurchaseInfo"><p>${item.price}</p> <button class = "buyButton" id = btn${item.id}> Köp! </button></div></div> `
+      )
+      .join("");
+  } else {
+    renderAll();
+  }
 }
